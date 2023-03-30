eb460255ed4020d3eba767aa2c29b6bb47253e2f
elizebeth8 <elizebethshaji8@gmail.com>
2023-03-30 21:04:36 +0530

    praty

--- lib/pages/aboutus.dart
+++ lib/pages/aboutus.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 class AboutUsPage extends StatelessWidget {
+   const AboutUsPage({super.key});
+   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:ecoconnect/pages/aboutus.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -37,12 +38,18 @@ class Homepage extends StatelessWidget {
                     Icons.menu,
                     color: Colors.black,
                     size: 30,
-                  )),
+                  )
+                  ),
               SizedBox(
                 width: 50,
               ),
               IconButton(
-                  onPressed: () {},
+                  onPressed: () {
+                     Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => AboutUsPage()),
+                      );
+                  },
                   tooltip: 'Login',
                   icon: Icon(
                     Icons.person,
